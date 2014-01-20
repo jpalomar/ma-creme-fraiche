43b5b2f75d125d6c43aac6c72a42f1ffa474c625
JR Palomar <jpalomar@netsocket.com>
2014-01-20 03:38:24 -0600

    some wip commits

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,12 +1,31 @@
-(function ($, _) {
-// function redirect () {
-//  location.href = 'http://blog.macremefraiche.com/';
-// }
-// _(redirect).delay(5000);
+
+(function ($, _, undefined) {
+
+// local namespace
+var GLOBAL = this;
+var localStorage = GLOBAL.localStorage;
 
 $('#showNavigation')
-.click(function (_evt){
-    $('#navigation').slideToggle(400);
+    .click(function (_evt){
+        $('#navigation').slideToggle(200, function(){
+            localStorage.setItem('isNavOpen', $(this).is(':visible') );
+        });
+
+    });
+
+
+// init page
+$(function($){
+
+    // retain nav state
+    if ( JSON.parse( localStorage.getItem('isNavOpen') ) ) {
+        $('#showNavigation').click();
+    }
+
+    // check if it's the first visit.
+    if ( ! JSON.parse( localStorage.getItem('hasVisited') ) ) {
+        localStorage.setItem('hasVisited', true );
+    }
 });
 
 }).call(this, this.jQuery, this._);
