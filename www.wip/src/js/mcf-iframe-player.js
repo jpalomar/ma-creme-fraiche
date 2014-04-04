a4c98c8fa97b0e7ca2667d0e9c421b0dba833215
JR Palomar <jpalomar@netsocket.com>
2014-04-04 13:52:45 -0500

    remove hoverZoom; move scripts around be less inline html; check in some minified stuff; add ion icons

---
+++ www.wip/src/js/mcf-iframe-player.js
@@ -0,0 +1,30 @@
+(function ($, _) {
+
+$(function() {
+    var $videos = $('iframe[src^="http://www.youtube.com"], iframe[src^="http://player.vimeo.com"], iframe[src^="https://w.soundcloud.com"]');
+    var $container = $('#footer .container');
+    var fn_vid_resize = _(function() {
+        var newWidth = $container.width();
+        // Resize all videos according to their own aspect ratio
+        $videos.each(function() {
+            var $vid = $(this);
+            $vid
+                .width('100%')
+                .height(newWidth * $vid.data('aspectRatio'))
+                ;
+        });
+    }).debounce(300);
+
+    // Figure out and save aspect ratio for each video
+    $videos.each(function() {
+        $(this)
+            .data('aspectRatio', this.height / this.width)
+            // and remove the hard coded width/height
+            .removeAttr('height width')
+            ;
+    });
+    // When the window is resized
+    $(window).resize(fn_vid_resize).resize();
+});
+
+}).call(this, this.jQuery, this._ );
