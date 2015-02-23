5a859be439d863fccab8c1c405724d8558ff0012
JR <jr@macremefraiche.com>
2015-02-22 18:56:59 -0600

    resolver;

--- src/assets/js/mcf.js
+++ src/assets/js/mcf.js
@@ -1,5 +1,5 @@
 require([
-    'jquery',
+    'bootstrap',
 
     // get the mcf front splash
     'js/mcf-front-splash'
@@ -9,10 +9,38 @@ require([
 ) { 'use strict';
 
 
-// init page
-$(function(){
-    splash.init();
-});
+function do_mcf_resolver( )
+{
+    // strip the utility classnames to avoid re-selection
+    function do_resolved_cleanup( )
+    {
+        $resolvers.removeClass( 'mcf-resolver mcf-resolved' );
+    }
 
+    var $resolvers = $( '.mcf-resolver' ).addClass( 'mcf-resolved' );
+
+    // do when transition ends or just invoke immediately
+    if ( $.support.transition )
+    {
+        $resolvers.one( 'bsTransitionEnd', do_resolved_cleanup );
+    }
+    else
+    {
+        do_resolved_cleanup( );
+    }
+}
+
+function do_doc_ready( )
+{
+    splash.init( );
+    $( document ).trigger( 'mcf-resolver' );
+}
+
+$( document )
+    // define
+    .on( 'mcf-resolver', do_mcf_resolver )
+    // init page
+    .ready( do_doc_ready )
+    ;
 
 }); // end mcf
