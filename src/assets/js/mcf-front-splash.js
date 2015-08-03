99116d5909c0f598c1757b8d05a4d687034af7c5
JR <jr@macremefraiche.com>
2015-08-03 01:11:19 -0500

    update slash routines;

--- src/assets/js/mcf-front-splash.js
+++ src/assets/js/mcf-front-splash.js
@@ -8,68 +8,85 @@ define([
     d3
 ) { 'use strict';
 
-    function do_tick() {
+    function place_and_update_tooltip (_data, _index) {
+        var xy = d3.mouse( document.documentElement );
+        $tooltip
+            .attr('data-time', _index)
+            .css({
+                'top': (xy[1] - $tooltip.height() * 1.5),
+                'left': (xy[0] - $tooltip.width() / 2),
+            });
+        fn_refreshtip();
+    }
 
-        function place_and_update_tooltip (_data, _index) {
-            var xy = d3.mouse(document.documentElement);
-            $tooltip
-                .attr('data-time', _index)
-                .css({
-                    'top': (xy[1] - $tooltip.height() * 1.5),
-                    'left': (xy[0] - $tooltip.width() / 2),
-                });
-            fn_refreshtip();
-        }
+    function hide_tooltip() {
+        $tooltip.removeClass('in');
+    }
 
-        function hide_tooltip() {
-            $tooltip.removeClass('in');
-        }
+    function toggle_tooltip(_data, _index) {
+        $tooltip.toggleClass('in');
+        place_and_update_tooltip.call(this, _data, _index);
+    }
 
-        function toggle_tooltip(_data, _index) {
-            $tooltip.toggleClass('in');
-            place_and_update_tooltip.call(this, _data, _index);
-        }
+    function show_tooltip(_data, _index) {
+        $tooltip.addClass('in');
+        place_and_update_tooltip.call(this, _data, _index);
+    }
 
-        function show_tooltip(_data, _index) {
-            $tooltip.addClass('in');
-            place_and_update_tooltip.call(this, _data, _index);
-        }
+    function do_update_data_model( _d3_selection ) {
 
         // store for reference
-        var previousValue = [];
+        var
+            previous_value = [],
+            map_previous_values = function( _data, _index ) {
+                previous_value[_index] = _data.value;
+            },
+            assign_previous_values = function( _data, _index ) {
+                _data.previous_value = previous_value[_index];
+            }
+            ;
+
         data_model_of_time = do_get_the_time();
 
-        d3_paths
-            .each(function (_data, _index) {
-                previousValue[_index] = _data.value;
-            })
-            .data(data_model_of_time)
-            .each(function (_data, _index) {
-                _data.previousValue = previousValue[_index];
-            });
+        _d3_selection
+            .each( map_previous_values )
+            .data( data_model_of_time )
+            .each( assign_previous_values )
+            ;
 
-        d3_paths.select('path')
+    }
+
+    function update_transition( _data ) {
+        var interpolateNumber = d3.interpolateNumber(_data.previous_value, _data.value);
+        return function (_tween) {
+            _data.value = interpolateNumber(_tween);
+            return d3_arc(_data);
+        };
+    }
+
+    function update_colors(_data) {
+        return d3_color(_data.value);
+    }
+
+    function do_tick() {
+
+        d3_paths
+            .call( do_update_data_model )
+            .select('path')
             .on('mousemove', show_tooltip )
             .on('mouseout', hide_tooltip )
             .on('mouseup', toggle_tooltip )
             .transition()
             .ease('back')
-            .attrTween('d', function (_data) {
-                var interpolateNumber = d3.interpolateNumber(_data.previousValue, _data.value);
-                return function (_tween) {
-                    _data.value = interpolateNumber(_tween);
-                    return d3_arc(_data);
-                };
-            })
-            .style('fill', function (_data) {
-                return d3_color(_data.value);
-            });
+            .attrTween('d', update_transition )
+            .style('fill', update_colors )
+            ;
 
-        if ($tooltip.is('.in')) {
+        if ( $tooltip.is('.in') ) {
             fn_refreshtip();
         }
 
-        timeout_reference = _(do_tick).delay(1000 - _.now() % 1000);
+        timeout_reference = _( do_tick ).delay( 1000 - _.now() % 1000 );
     }
 
     function do_get_the_time() {
@@ -109,43 +126,49 @@ define([
                 'height': _height,
             })
             .append('g')
+            .classed('svg-clock-g mcf-fade-in', true)
             .attr({
-                'class': 'svg-clock-g mcf-fade-in',
                 'transform': 'translate(' + _width / 2 + ',' + _height / 2 + ')',
-            });
+            })
+            ;
 
         d3_paths = d3_group.selectAll('g')
             .data(do_get_the_time)
-            .enter().append('g');
+            .enter().append('g')
+            ;
 
         d3_paths.append('path');
 
-        radius = Math.min(_width, _height) / 2;
+        radius = Math.min( _width, _height ) / 2;
     }
 
     function do_empty_the_clock() {
         // empty
-        this.selectAll('*').remove();
+        this.selectAll('*')
+            .remove()
+            ;
     }
 
     function fn_refreshtip() {
         // var index = $tooltip.attr('data-time');
         // var text = data_model_of_time[index].text;
         // $tooltip_content.text( text );
-        $tooltip_content.text(data_model_of_time[$tooltip.attr('data-time')].text);
+        $tooltip_content
+            .text( data_model_of_time[ $tooltip.attr('data-time') ].text )
+            ;
     }
 
     function do_view_update_of_clock() {
-        console.log('do_view_update_of_clock');
+        // console.log('do_view_update_of_clock');
         clearTimeout(timeout_reference);
         // empty
         d3_svg
-            .call(do_empty_the_clock)
-            .call(do_create_the_clock, $container.width(), $container.height())
+            .call( do_empty_the_clock )
+            .call( do_create_the_clock, $container.width(), $container.height())
             ;
 
         d3.transition()
-            .each(do_tick)
+            .each( do_tick )
             ;
     }
 
@@ -168,7 +191,7 @@ define([
         return (_data.index + spacing) * radius;
     }
 
-    function initialize_the_front_splash() {
+    function do_init() {
         $container = $(selector);
         do_format_second = d3_time_format('%S s');
         do_format_minute = d3_time_format('%I:%M %p');
@@ -176,7 +199,7 @@ define([
         do_format_day = d3_time_format('%a, %b %d');
         do_format_date = d3_time_format('%b %d');
         do_format_month = d3_time_format('%b %Y');
-        do_resize_svg_clock = _(do_view_update_of_clock).debounce(200);
+        do_resize_svg_clock = _( do_view_update_of_clock ).debounce(200);
 
         d3_color = d3.scale.linear()
             .range(['hsl(99,22%,75%)', 'hsl(350,71%,67%)'])
@@ -196,7 +219,7 @@ define([
 
         $tooltip = $($.fn.tooltip.Constructor.DEFAULTS.template)
             .addClass( 'mcf-tooltip top fade' )
-            .prependTo(document.body)
+            .prependTo( document.body )
             ;
 
         $tooltip_content = $tooltip.find('.tooltip-inner');
@@ -208,7 +231,8 @@ define([
             ;
     }
 
-    var spacing = 0.09,
+    var
+        spacing = 0.09,
         selector = '.embed-front-splash',
         d3_time_format = d3.time.format,
         do_resize_svg_clock,
@@ -231,7 +255,7 @@ define([
         ;
 
     return {
-        'init' : _.once(initialize_the_front_splash)
+        'init' : _.once( do_init )
     };
 
 });
